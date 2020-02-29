7e800af4a29224b27c28ff8c9fc011df612e2153
biswomohan <biswomohan.pattanaik@gmail.com>
2020-02-29 19:00:24 +0530

    Adding config client

---
+++ cloud-client-config/src/main/java/com/biswo/spring/cloud/CloudClientConfigApplication.java
@@ -0,0 +1,13 @@
+package com.biswo.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudClientConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudClientConfigApplication.class, args);
+	}
+
+}
---
+++ cloud-client-config/src/test/java/com/biswo/spring/cloud/CloudClientConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biswo.spring.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudClientConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
