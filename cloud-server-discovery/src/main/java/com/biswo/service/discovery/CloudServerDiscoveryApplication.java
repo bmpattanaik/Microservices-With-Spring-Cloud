35f286056ca872d59e5115991b30013663672e52
bmpattanaik <biswomohan.pattanaik@gmail.com>
2020-03-07 21:00:28 +0530

    Service discovery added.

---
+++ cloud-server-discovery/src/main/java/com/biswo/service/discovery/CloudServerDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.biswo.service.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerDiscoveryApplication.class, args);
+	}
+
+}
---
+++ cloud-server-discovery/src/test/java/com/biswo/service/discovery/CloudServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biswo.service.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
